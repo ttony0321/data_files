9aa3192a5ef0a51d96462290ae60452cc1dd3f20
ttony0321 <48801180+ttony0321@users.noreply.github.com>
2021-06-22 00:53:42 +0900

    Add files via upload

--- test5.py
+++ test5.py
@@ -1,10 +1,12 @@
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode, quote_plus, unquote
+from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import csv
 import json
 import xmltodict
 import requests
+import pandas as pd
 #import urllib2
 
 #Open API Url 생성
@@ -26,7 +28,7 @@ file.write(json.dumps(result_data))
 # xml_result_data = get_xml_data.text
 
 ##CSV FILE
-print(result_data['data'][0].keys())
+#print(result_data['data'][0].keys())
 
 with open('vaccin.csv', 'w', encoding='utf-8')as f:
     wr = csv.DictWriter(f, fieldnames = result_data['data'][0].keys())
@@ -48,29 +50,80 @@ f.write(response_body)
 
 
 #XML => JSON
+#CONVERTING
 tojson = xmltodict.parse(response_body)
+#JSON DUMPING
 json_obj = json.dumps(tojson)
+#SAVING
 covid_file = open('covid.json', 'w+')
-covid_file.write(json.dumps(json_obj))
-print(json_obj)
+covid_file.write(json_obj)
+f.close()
 
-#XML => CSV
-response_body = urlopen(request).read()
-data = str(response_body, 'utf-8')
-tree = ET.parse(data)
-root = tree.getroot()
-for elem in root:
-    accDefRate = elem.find("accDefRate").text
-    accExamCnt = elem.find("accExamCnt").text
-    accExamCompCnt = elem.find("accExamCompCnt").text
-    careCnt = elem.find("careCnt").text
-    clearCnt = elem.find("clearCnt").text
-    createDt = elem.find("createDt").text
-    deathCnt = elem.find("deathCnt").text
-    decideCnt = elem.find("decideCnt").text
-    examCnt = elem.find("examCnt").text
-    resutlNegCnt = elem.find("resutlNegCnt").text
-    seq = elem.find("seq").text
-    stateDt = elem.find("stateDt").text
-    stateTime = elem.find("stateTime").text
-    updateDt = elem.find("updateDt").text
+
+# #XML => CSV (pandas)
+#tree = ET.parse('covid.xml')
+df = pd.DataFrame(columns=['accDefRate', 'accExamCnt', 'accExamCompCnt', 'careCnt', 'clearCnt', 'createDt', 'deathCnt', 'decideCnt', 'examCnt', 'resutlNegCnt', 'seq', 'stateDt', 'stateTime', 'updateDt'])
+xml_url = url + queryParams
+data = urlopen(xml_url).read()
+soup = BeautifulSoup(data, "html.parser")
+k = 0
+for i in soup.findAll('item'):
+    df.loc[k] = [
+        i.accDefRate.text,
+        i.accExamCnt.text,
+        i.accExamCompCnt.text,
+        i.careCnt.text,
+        i.clearCnt.text,
+        i.createDt.text,
+        i.deathCnt.text,
+        i.decideCnt.text,
+        i.examCnt.text,
+        i.resutlNegCnt.text,
+        i.seq.text,
+        i.stateDt.text,
+        i.stateTime.text,
+        i.updateDt.text
+    ]
+    k +=1
+df.to_csv('covid.csv', encoding='utf-8')
+
+# cols = ['accDefRate', 'accExamCnt', 'accExamCompCnt', 'careCnt', 'clearCnt', 'createDt', 'deathCnt', 'decideCnt', 'examCnt', 'resutlNegCnt', 'seq', 'stateDt', 'stateTime', 'updateDt']
+# rows = []
+# response_body = urlopen(request).read()
+# data = str(response_body, 'utf-8')
+# tree = ET.parse('covid.xml')
+# root = tree.getroot()
+# for elem in root:
+#     accDefRate = elem.find("accDefRate").text
+#     accExamCnt = elem.find("accExamCnt").text
+#     accExamCompCnt = elem.find("accExamCompCnt").text
+#     careCnt = elem.find("careCnt").text
+#     clearCnt = elem.find("clearCnt").text
+#     createDt = elem.find("createDt").text
+#     deathCnt = elem.find("deathCnt").text
+#     decideCnt = elem.find("decideCnt").text
+#     examCnt = elem.find("examCnt").text
+#     resutlNegCnt = elem.find("resutlNegCnt").text
+#     seq = elem.find("seq").text
+#     stateDt = elem.find("stateDt").text
+#     stateTime = elem.find("stateTime").text
+#     updateDt = elem.find("updateDt").text
+
+#     rows.append({
+#         'accDefRate': accDefRate,
+#         'accExamCnt': accExamCnt,
+#         'accExamCompCnt': accExamCompCnt,
+#         'careCnt': careCnt,
+#         'clearCnt': clearCnt,
+#         'createDt': createDt,
+#         'deathCnt': deathCnt,
+#         'decideCnt': decideCnt,
+#         'examCnt': examCnt,
+#         'resutlNegCnt': resutlNegCnt,
+#         'seq': seq,
+#         'stateDt': stateDt,
+#         'stateTime': stateTime,
+#         'updateDt': updateDt,
+#     })
+#     df = pd.DataFrane(rows, columns=cols)
+#     df.to_csv('covid.csv')
